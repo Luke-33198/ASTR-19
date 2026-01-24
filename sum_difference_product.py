6839b045d6ff782d4704e3cd87f413c0bb24e1a0
Luke-33198 <lzidbeck@ucsc.edu>
2026-01-24 12:24:00 -0800

    Add files via upload

---
+++ sum_difference_product.py
@@ -0,0 +1,8 @@
+print(f"The sum of the two floats: {3.5 + 1.25} type: {type(3.5+1.25)}")
+print(f"The difference of the two integers: {3-1} type: {type(3-1)}")
+print(f"The product of one float and one integer: {2.3*6} type: {type(2.3*6)}")
+
+
+
+
+
